fb24f291844aa27df8266419e9d9c915b5b1e4ea
Matt Oatley <Matthew.Oatley225@gmail.com>
2020-07-06 14:47:09 -0500

    Finished First set of ToDo's, working on the implementation

---
+++ ConsoleUI/Car.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleUI
+{
+    public class Car : Vehicle
+    {
+        bool hasTrunk { get; set; } = true;
+
+
+
+
+    }
+}
